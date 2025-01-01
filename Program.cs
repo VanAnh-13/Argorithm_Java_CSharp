585064f20a3da88f5325f49aebdf880bc213c016
Van Anh <levananh13062004@gmail.com>
2025-01-02 00:50:48 +0700

    git commit -m "Leetcode, top interview: Roman to Integer"

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Algorithm
+{
+    class RomanToInteger
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(RomanToInt("III"));
+        }
+
+        public static int RomanToInt(string s)
+        {
+            Dictionary<char, int> roman = new Dictionary<char, int>()
+            {
+                {
+                    'I', 1
+                },
+                {
+                    'V', 5
+                },
+                {
+                    'X', 10
+                },
+                {
+                    'L', 50
+                },
+                {
+                    'C', 100
+                },
+                {
+                    'D', 500
+                },
+                {
+                    'M', 1000
+                }
+            };
+
+            int values = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (i + 1 < s.Length && roman[s[i + 1]] > roman[s[i]])
+                {
+                    values -= roman[s[i]];
+                }
+                else
+                {
+                    values += roman[s[i]];
+                }
+            }
+
+            return values;
+        }
+    }
+}
