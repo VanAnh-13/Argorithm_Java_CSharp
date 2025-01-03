d0bb56f21091426e4cb075a080a37ff90e405add
Van Anh <levananh13062004@gmail.com>
2025-01-03 23:52:23 +0700

    Leetcode, top interview: Length of Last Word

---
+++ LengthOfLastWord.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Algorithm
+{
+    public class Solution
+    {
+        private  int LengthOfLastWord(string s)
+        {
+            return Regex.Replace(s.Trim(), @"\s+", " ").Split(' ').Last().Length;
+        }
+    }
+}
