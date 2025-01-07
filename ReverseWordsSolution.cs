39fd6c4c784b920de5eb26f283b6ecbb980aa305
Van Anh <levananh13062004@gmail.com>
2025-01-07 17:25:59 +0700

    Leetcode, top interview: Reverse Words in a String

---
+++ ReverseWordsSolution.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Algorithm
+{
+    public class ReverseWordsSolution
+    {
+        public string ReverseWords(string s)
+        {
+            s = s.Trim();
+            s = Regex.Replace(s, @"\s+", " ");
+            List<string> splitString = new List<string>(s.Split(' '));
+            splitString.Reverse();
+            
+            return string.Join(" ", splitString);
+        }
+    }
+}
