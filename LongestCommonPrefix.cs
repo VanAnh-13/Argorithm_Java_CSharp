c4202cdec6aeacc0ff0f6b67581812e9bdb2748c
Van Anh <levananh13062004@gmail.com>
2025-01-07 17:25:18 +0700

    Leetcode, top interview: Longest Common Prefix

---
+++ LongestCommonPrefix.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Algorithm
+{
+    public class LongestCommonPrefixSolution
+    {
+        public string LongestCommonPrefix(string[] strs)
+        {
+            if (strs.Length <= 0)
+            {
+                return "";
+            }
+
+            string compareString = strs[0];
+
+            for (int i = 0; i < compareString.Length; i++)
+            {
+                char compareChar = compareString[i];
+
+                for (int j = 1; j < strs.Length; j++)
+                {
+                    if (i >= strs[j].Length || compareChar != strs[j][i])
+                    {
+                        return compareString.Substring(0, i);
+                    }
+                }
+            }
+
+            return compareString;
+        }
+    }
+}
