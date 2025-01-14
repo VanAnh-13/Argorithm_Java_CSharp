558f414e45fa32ed54ece16677a7a1e7a4316f0b
Van Anh <levananh13062004@gmail.com>
2025-01-15 00:33:56 +0700

    Leetcode, top interview: Find The Index Of String

---
+++ FindTheIndexOfString.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Algorithm
+{
+    public class FindTheIndexOfString
+    {
+        public int StrStr(string haystack, string needle)
+        {
+            Match textMatch = Regex.Match(haystack, needle);
+            return textMatch.Success ? textMatch.Index : -1;
+        }
+    }
+}
