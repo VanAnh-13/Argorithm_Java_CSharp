edf51388078c82785999c545f4da207a17dda906
Van Anh <levananh13062004@gmail.com>
2025-01-15 00:34:22 +0700

    Leetcode, top interview: Is Subsequence String

---
+++ IsSubsequenceSolution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace Algorithm
+{
+    public class IsSubsequenceSolution
+    {
+        public bool IsSubsequence(string s, string t)
+        {
+            int indexMatch = -1;
+
+            return s.Select(c => indexMatch = t.IndexOf(value: c, startIndex: indexMatch + 1))
+                .All(i => i != -1);
+        }
+    }
+}
