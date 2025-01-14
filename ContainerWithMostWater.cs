6bea6f04a84191fc31ccac623c208c72f451a87f
Van Anh <levananh13062004@gmail.com>
2025-01-15 00:33:28 +0700

    Leetcode, top interview: Container With Most Water

---
+++ ContainerWithMostWater.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Algorithm
+{
+    public class ContainerWithMostWater
+    {
+        public int MaxArea(int[] height) {
+            int left = 0;
+            int right = height.Length - 1;
+            int maxArea = 0;
+
+            while(left < right) {
+                int w = right - left;
+                int h = Math.Min(height[left], height[right]);
+                maxArea = Math.Max(w * h, maxArea);
+
+                if(height[left] < height[right]) {
+                    left++;
+                }
+
+                else {
+                    right--;
+                }
+            }
+
+            return maxArea;
+        }
+    }
+}
