6c94f862dcc2126ae9a73cf5a305e963bf999daa
Van Anh <levananh13062004@gmail.com>
2025-01-27 22:01:03 +0700

    Leetcode, top interview: Valid Anagram

---
+++ src/ValidAnagram.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class ValidAnagram {
+    public boolean isAnagram(String s, String t) {
+        char[] sArray = s.toCharArray();
+        char[] tArray = t.toCharArray();
+        Arrays.sort(sArray);
+        Arrays.sort(tArray);
+
+        return Arrays.equals(sArray, tArray);
+    }
+}
