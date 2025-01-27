757a21707a742d5737ad98d918bfc0ea4cb8e84a
Van Anh <levananh13062004@gmail.com>
2025-01-27 22:03:57 +0700

    Leetcode, top interview: Maximum Depth Of Binary Tree

---
+++ src/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,31 @@
+public class MaximumDepthOfBinaryTree {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int maxLeft = maxDepth(root.left);
+        int maxRight = maxDepth(root.right);
+
+        return 1 + Math.max(maxLeft, maxRight);
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
