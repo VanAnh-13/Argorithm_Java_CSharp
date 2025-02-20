d8b98ee9d493bb690eef601e0afd41b46f070962
Van Anh <levananh13062004@gmail.com>
2025-02-20 22:57:01 +0700

    Leetcode, top interview: Minimum Size SubarraySum

---
+++ MinimumSizeSubarraySum.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Algorithm
+{
+    public class MinimumSizeSubarraySum
+    {
+        public int MinSubArrayLen(int target, int[] nums)
+        {
+            int left = 0, currentSum = 0, currentWindow = Int32.MaxValue;
+
+            for (int right = 0; right < nums.Length; right++)
+            {
+                currentSum += nums[right];
+
+                while (currentSum >= target)
+                {
+                    currentWindow = Math.Min(currentWindow, right - left + 1);
+                    currentSum -= nums[left++];
+                }
+            }
+
+            return currentWindow != Int32.MaxValue ? currentWindow : 0;
+        }
+    }
+}
