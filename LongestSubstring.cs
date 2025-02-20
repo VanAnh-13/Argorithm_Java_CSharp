1f79092e75fe13b41661211d2ef696a5e7a73508
Van Anh <levananh13062004@gmail.com>
2025-02-20 22:56:44 +0700

    Leetcode, top interview: Longest Substring

---
+++ LongestSubstring.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithm
+{
+    public class LongestSubstring
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            int left = 0, currentWindow = 0;
+            HashSet<char> characterCheck = new HashSet<char>();
+
+            for (int right = 0; right < s.Length; right++)
+            {
+                while (characterCheck.Contains(s[right]))
+                {
+                    characterCheck.Remove(s[left]);
+                    left++;
+                }
+
+                currentWindow = Math.Max(currentWindow, right - left + 1);
+                characterCheck.Add(s[right]);
+            }
+
+            return currentWindow;
+        }
+    }
+}
