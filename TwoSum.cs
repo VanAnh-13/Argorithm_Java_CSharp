5dbcf30057eb7cf9b009b025b2b8cbd359954290
Van Anh <levananh13062004@gmail.com>
2025-02-20 22:57:28 +0700

    Leetcode, top interview: Two Sum

---
+++ TwoSum.cs
@@ -0,0 +1,31 @@
+﻿namespace Algorithm
+{
+    public class FindTwoSum
+    {
+        public int[] TwoSum(int[] numbers, int target)
+        {
+            int right = numbers.Length - 1, left = 0;
+
+            while (left < right)
+            {
+                int sum = numbers[left] + numbers[right];
+
+                if (sum == target)
+                {
+                    return new int[] { left + 1, right + 1 };
+                }
+
+                if (sum < target)
+                {
+                    left++;
+                }
+                else
+                {
+                    right--;
+                }
+            }
+
+            return null;
+        }
+    }
+}
